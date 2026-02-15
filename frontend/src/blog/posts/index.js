9bb7ec65caaf56ee279dd497dfca5ab8a90bf086
brenodinizcorrea <brenodinizcorrea@gmail.com>
2026-02-15 19:44:09 -0300

    feat-post-example

--- frontend/src/blog/posts/index.js
+++ frontend/src/blog/posts/index.js
@@ -1,21 +1,13 @@
 // src/blog/posts/index.js
 import Post1 from "./Post1";
-import Post2 from "./Post2"; // futuramente
 
 const postsMap = {
   1: { 
     component: Post1, 
-    title: "Aprendendo Python", 
-    learnings: "Você vai aprender conceitos básicos de Python, variáveis, loops e funções.", 
+    title: "Example", 
+    learnings: "Example", 
     readTime: "5 minutes",
-    tags: ["Python", "Iniciante", "Programação"]  // <- novas tags
-  },
-  2: { 
-    component: Post2, 
-    title: "Aprendendo Statistics", 
-    learnings: "Você vai aprender conceitos básicos de Python, variáveis, loops e funções.", 
-    readTime: "5 minutes",
-    tags: ["Statistics"]  // <- novas tags
+    tags: ["Python"]  // <- novas tags
   },
   // 2: { component: Post2, title: "Outro Post", learnings: "...", readTime: "X minutes", tags: ["Tag1", "Tag2"] },
 };
